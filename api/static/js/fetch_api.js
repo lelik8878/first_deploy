de39790e6007d6128b7018013f333ce3e2d167a6
lelik8878 <lelik8878@gmail.com>
2024-10-17 19:25:56 +0300

    17.10

--- api/static/js/fetch_api.js
+++ api/static/js/fetch_api.js
@@ -1,1 +1,8 @@
 alert('johan')
+document.getElementById('name_button').addEventListener("click", function (){
+    let name_value = document.getElementById('name_value').value;
+    document.getElementById('printed_name').innerText =`{name : '` +name_value +`'}` //+`${name_value}`
+    const new_json = {name: name_value}
+    const print_json = JSON.stringify(new_json, null, 2)
+    document.getElementById('printed_json').textContent = print_json
+    })
